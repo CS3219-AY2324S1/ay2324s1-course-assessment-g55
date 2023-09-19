9fff612e71b8f86a9f5bc6c173e1a2ff0c3de665
malwaregarry <malwaregarry@gmail.com>
2023-09-19 18:42:12 +0800

    chore: set up redirect from main page (for a1)

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,9 @@
+import { redirect } from "next/navigation";
+
 export default function Home() {
-  return (
+  redirect('/questions'); // redirect for assignment 1
+  /* return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
     </main>
-  )
+  ) */
 }
