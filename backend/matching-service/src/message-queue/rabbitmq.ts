854263d3c9e793a6cd9045d7674479a9e9e82350
Francis <linux@francisyzy.com>
2023-09-27 16:08:56 +0000

    chore: prettier everything
    previously was waiting for a plugin that is not installed

--- backend/matching-service/src/message-queue/rabbitmq.ts
+++ backend/matching-service/src/message-queue/rabbitmq.ts
@@ -11,15 +11,17 @@ class RabbitMQService {
   async initialize() {
     try {
       // Connect to RabbitMQ server
-      if (rabbitmqUrl===undefined) {
-          throw new Error(  'RABBITMQ_URL is not defined in the .env file.')
-        }
-        this.connection = await amqp.connect(rabbitmqUrl);
+      if (rabbitmqUrl === undefined) {
+        throw new Error('RABBITMQ_URL is not defined in the .env file.');
+      }
+      this.connection = await amqp.connect(rabbitmqUrl);
       // Create a channel
       this.channel = await this.connection.createChannel();
 
       // Declare the exchange and queues
-      await this.channel.assertExchange('matchmaking', 'direct', { durable: true });
+      await this.channel.assertExchange('matchmaking', 'direct', {
+        durable: true,
+      });
       await this.channel.assertQueue('easy', { durable: true });
       await this.channel.assertQueue('medium', { durable: true });
       await this.channel.assertQueue('hard', { durable: true });
@@ -30,7 +32,7 @@ class RabbitMQService {
       await this.channel.bindQueue('hard', 'matchmaking', 'hard');
 
       console.log('RabbitMQ initialized successfully');
-    } catch (error:any) {
+    } catch (error: any) {
       console.error('Error initializing RabbitMQ:', error.message);
     }
   }
@@ -44,12 +46,18 @@ class RabbitMQService {
     try {
       await this.channel.sendToQueue(queue, Buffer.from(message));
       console.log(`Message sent to queue "${queue}": ${message}`);
-    } catch (error:any) {
-      console.error(`Error sending message to queue "${queue}":`, error.message);
+    } catch (error: any) {
+      console.error(
+        `Error sending message to queue "${queue}":`,
+        error.message
+      );
     }
   }
 
-  async consumeMessage(queue: string, callback: (message: amqp.Message | null) => void) {
+  async consumeMessage(
+    queue: string,
+    callback: (message: amqp.Message | null) => void
+  ) {
     if (!this.channel) {
       console.error('RabbitMQ channel not initialized');
       return;
@@ -71,18 +79,21 @@ class RabbitMQService {
             const players = [player, matchedPlayer];
 
             // Create a room and send players to it
-          //   this.createRoomAndSendPlayers(queue, players);
+            //   this.createRoomAndSendPlayers(queue, players);
           } else {
             // No match found, requeue the message
             await this.channel!.nack(message);
           }
         }
       });
-    } catch (error:any) {
-      console.error(`Error consuming messages from queue "${queue}":`, error.message);
+    } catch (error: any) {
+      console.error(
+        `Error consuming messages from queue "${queue}":`,
+        error.message
+      );
     }
   }
-  
+
   // Helper function to determine the appropriate match queue based on the player's queue
   private getMatchQueue(queue: string): string {
     switch (queue) {
@@ -96,30 +107,35 @@ class RabbitMQService {
         throw new Error(`Invalid queue: ${queue}`);
     }
   }
-  
-// Helper function to find a match for a player in the specified match queue
-private async findMatch(player: any, matchQueue: string): Promise<any | null> {
+
+  // Helper function to find a match for a player in the specified match queue
+  private async findMatch(
+    player: any,
+    matchQueue: string
+  ): Promise<any | null> {
     if (!this.channel) {
       console.error('RabbitMQ channel not initialized');
       return null;
     }
-  
+
     try {
       // Retrieve messages from the match queue
       const result = await this.channel.get(matchQueue);
-  
+
       if (result !== false && result.content) {
         const matchedPlayer = JSON.parse(result.content.toString());
         return matchedPlayer;
       }
-  
+
       return null;
-    } catch (error:any) {
-      console.error(`Error finding a match in queue "${matchQueue}":`, error.message);
+    } catch (error: any) {
+      console.error(
+        `Error finding a match in queue "${matchQueue}":`,
+        error.message
+      );
       return null;
     }
   }
-  
 }
 
 export default new RabbitMQService();
--- backend/matching-service/src/websocket/socket-handler.ts
+++ backend/matching-service/src/websocket/socket-handler.ts
@@ -14,25 +14,29 @@ export function initializeSocketHandlers(io: Server) {
     });
 
     // Handle matchmaking requests
-    socket.on('joinMatchmaking', (data: { userId: string, difficulty: string }) => {
-      const { userId, difficulty } = data;
+    socket.on(
+      'joinMatchmaking',
+      (data: { userId: string; difficulty: string }) => {
+        const { userId, difficulty } = data;
 
-      // Implement matchmaking logic
-      matchmakingLogic.joinMatchmaking(userId, difficulty)
-        .then((match) => {
-          if (match) {
-            // Notify the user that a match has been found
-            socket.emit('matchFound', match);
-          } else {
-            // Notify the user that they are in the matchmaking queue
-            socket.emit('waitingForMatch');
-          }
-        })
-        .catch((error) => {
-          // Handle errors and notify the user
-          socket.emit('matchmakingError', { message: error.message });
-        });
-    });
+        // Implement matchmaking logic
+        matchmakingLogic
+          .joinMatchmaking(userId, difficulty)
+          .then((match) => {
+            if (match) {
+              // Notify the user that a match has been found
+              socket.emit('matchFound', match);
+            } else {
+              // Notify the user that they are in the matchmaking queue
+              socket.emit('waitingForMatch');
+            }
+          })
+          .catch((error) => {
+            // Handle errors and notify the user
+            socket.emit('matchmakingError', { message: error.message });
+          });
+      }
+    );
 
     // Handle other WebSocket events as needed
     // ...
