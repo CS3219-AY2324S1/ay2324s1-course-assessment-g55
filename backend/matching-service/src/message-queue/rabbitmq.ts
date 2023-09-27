070a47e2e4f9f6056a1682838e08615c1289948a
Francis <linux@francisyzy.com>
2023-09-27 16:07:25 +0000

    feat: init message consumer

--- backend/matching-service/src/message-queue/rabbitmq.ts
+++ backend/matching-service/src/message-queue/rabbitmq.ts
@@ -49,20 +49,77 @@ class RabbitMQService {
     }
   }
 
-  async consumeQueue(queue: string, callback: (message: amqp.Message | null) => void) {
+  async consumeMessage(queue: string, callback: (message: amqp.Message | null) => void) {
     if (!this.channel) {
       console.error('RabbitMQ channel not initialized');
       return;
     }
 
     try {
-      await this.channel.consume(queue, (message) => {
-        callback(message);
+      // Consume messages from the specified queue
+      await this.channel.consume(queue, async (message) => {
+        if (message) {
+          const player = JSON.parse(message.content.toString());
+
+          // Find a match for the player based on difficulty level
+          const matchQueue = this.getMatchQueue(queue);
+          const matchedPlayer = await this.findMatch(player, matchQueue);
+
+          if (matchedPlayer) {
+            // Remove matched players from the queue
+            await this.channel!.ack(message);
+            const players = [player, matchedPlayer];
+
+            // Create a room and send players to it
+          //   this.createRoomAndSendPlayers(queue, players);
+          } else {
+            // No match found, requeue the message
+            await this.channel!.nack(message);
+          }
+        }
       });
     } catch (error:any) {
-      console.error(`Error consuming queue "${queue}":`, error.message);
+      console.error(`Error consuming messages from queue "${queue}":`, error.message);
     }
   }
+  
+  // Helper function to determine the appropriate match queue based on the player's queue
+  private getMatchQueue(queue: string): string {
+    switch (queue) {
+      case 'easy':
+        return 'easy';
+      case 'medium':
+        return 'medium';
+      case 'hard':
+        return 'hard';
+      default:
+        throw new Error(`Invalid queue: ${queue}`);
+    }
+  }
+  
+// Helper function to find a match for a player in the specified match queue
+private async findMatch(player: any, matchQueue: string): Promise<any | null> {
+    if (!this.channel) {
+      console.error('RabbitMQ channel not initialized');
+      return null;
+    }
+  
+    try {
+      // Retrieve messages from the match queue
+      const result = await this.channel.get(matchQueue);
+  
+      if (result !== false && result.content) {
+        const matchedPlayer = JSON.parse(result.content.toString());
+        return matchedPlayer;
+      }
+  
+      return null;
+    } catch (error:any) {
+      console.error(`Error finding a match in queue "${matchQueue}":`, error.message);
+      return null;
+    }
+  }
+  
 }
 
 export default new RabbitMQService();
