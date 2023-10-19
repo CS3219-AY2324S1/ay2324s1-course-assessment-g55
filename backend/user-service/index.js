67b5ff4e367a1a424a1a0e1ec2e1a6c3a042db4f
LimAiLin <limailin2009@gmail.com>
2023-10-20 00:25:25 +0800

    backend user-service

--- backend/user-service/index.js
+++ backend/user-service/index.js
@@ -1,34 +1,49 @@
-const { PrismaClient } = require('@prisma/client');
+// import the dependencies required for Express.js
+const express = require('express')
+// import the dependencies required for cors
+const cors = require('cors')
 
-const prisma = new PrismaClient()
+// the config() function allows for reading of the .env file
+const dotenv = require('dotenv').config()
 
-const express = require('express');
-const cors = require('cors');
-const dotenv = require('dotenv').config();
-const app = express();
+// initialize the Express.js application
+// store it in the app variable
+const app = express()
 
-// Middleware to allow cross-origin requests
-app.use(cors());
+// allow cross-origin requests to reach the Expres.js server
+// from localhost:3000, which is your frontend domain
+app.options(
+  '*',
+  cors({
+    origin: 'http://localhost:3000',
+    optionsSuccessStatus: 200,
+  }),
+)
+app.use(cors())
 
-// Middleware to parse JSON data in request body
-app.use(express.json());
+// allow JSON data in request body to be parsed
+app.use(express.json())
+// allow URL-encoded data in request body to be parsed
+app.use(express.urlencoded({ extended: false }))
 
-// Middleware to parse URL-encoded data in request body
-app.use(express.urlencoded({ extended: false }));
+const PORT = process.env.PORT || 8080
 
-const PORT = process.env.PORT;
-
+// configure the Express.js application to run at port 8080
+// since you will be running this application on your computer (localhost),
+// the backend server will be running at http://localhost:8080
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}...`);
-});
+  console.log(`Server is running on port ${PORT}...`)
+})
 
-const { User, Profile } = prisma;
+// use the address router to handle requests
+// at http://localhost:8080/api/users
+app.use('/api/users', require('./routes/routes'))
 
-// Use your Prisma models for handling routes (adjust as per your model structure)
-app.use('/api/users', require('./routes/routes')(User, Profile));
-
+// when a GET request is made to http://localhost:8080/,
+// the response will be { message: 'Hello World' } in JSON format
 app.get('/', (req, res) => {
-  res.json({ message: 'Hello World' });
-});
+  res.json({ message: 'Hello World' })
+})
 
-module.exports = app;
+// export Express.js application to be used elsewhere
+module.exports = app
