45ee6c18f8d8b54c2e1c189b969645f516f152dc
LimAiLin <limailin2009@gmail.com>
2023-11-05 22:19:16 +0800

    addition of home buttons

--- frontend/src/app/questions/page.tsx
+++ frontend/src/app/questions/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Box, Flex } from '@chakra-ui/react';
+import { Box, Flex, Link, Button } from '@chakra-ui/react';
 import { QuestionList } from './question-list';
 import { QuestionForm } from './question-form';
 import { useGetQuestions } from './api';
@@ -10,11 +10,18 @@ export default function QuestionsPage() {
 
   return (
     <main>
+      <Flex justifyContent="flex-start" p={4}>
+        <Link href="/">
+          <Button colorScheme="gray" mr={4}>
+            Home
+          </Button>
+        </Link>
+      </Flex>
       <Flex direction='column' align='center' className='gap-4'>
-        <Box w='60%'>
+        <Box w='60%' p="4">
           <QuestionForm questions={questions ?? []} />
         </Box>
-        <Box w='80%'>
+        <Box w='80%' p="4">
           <QuestionList questions={questions ?? []} />
         </Box>
       </Flex>
