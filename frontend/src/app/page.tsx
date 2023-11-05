b54d349781bf1347357e3cf0f4a25289b4fd2e65
LimAiLin <limailin2009@gmail.com>
2023-11-05 22:18:07 +0800

    home page to get to user-service and question-service

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,28 @@
-import { redirect } from "next/navigation";
+import Link from 'next/link';
+import { Box, Button, Heading } from '@chakra-ui/react';
 
 export default function Home() {
-  redirect('/login'); // redirect for assignment 2
-  /* return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    </main>
-  ) */
+  return (
+    <Box p="4">
+      <Heading as="h1" size="xl" mb="4">
+        Assignment 2
+      </Heading>
+      <Box p="4">
+        <Link href="/questions">
+          <Box mb="2">
+            <Button colorScheme="teal">
+              Question Repository
+            </Button>
+          </Box>
+        </Link>
+        <Link href="/login">
+          <Box mb="2">
+            <Button colorScheme="teal" mb="2">
+              User Profile Management
+            </Button>
+          </Box>
+        </Link>
+      </Box>
+    </Box>
+  );
 }
